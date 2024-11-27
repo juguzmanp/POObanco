750737db786d43ee9eec39ec56ea0c43f24ea7ac
David-294 <jedalvaradogu@unal.edu.co>
2024-11-26 22:15:18 -0500

    Update EjemploBanco.java

--- EjemploBanco.java
+++ EjemploBanco.java
@@ -7,6 +7,17 @@ package com.mycompany.ejemplobanco;
 import java.util.*;
 public class EjemploBanco {
   public static void main(String[] args) {
-    
-    
+        Cliente Nestor = new Cliente();
+        Cuenta cuentaN = new Cuenta(Nestor);
+        Cliente Juan = new Cliente();
+        Cuenta cuentaJ = new Cuenta(Juan); 
+        int operacion;
+        operacion = Nestor.bienvenida();
+        if(operacion == 1){
+        Nestor.retirar();
+        }
+        if(operacion == 2){
+        Nestor.consignar();}
+        else{Nestor.Transferir();}
+    }
 }
