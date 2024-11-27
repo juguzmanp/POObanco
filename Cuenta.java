523378d2295e2c7f4680c554ecb7f0669f73b670
juguzmanp <juguzmanp@unal.edu.co>
2024-11-26 22:36:14 -0500

    Update Cuenta.java

--- Cuenta.java
+++ Cuenta.java
@@ -14,72 +14,11 @@ public class Cuenta {
     private String Titular;
     private double ID;
     private double Saldo;
-    
-    boolean consignacion(int x){
-        
-        boolean peticion = true;
-        
-        if(peticion == true){
-                Saldo = Saldo + x;
-                System.out.println("Se se realizo existosamente la consignacion");
-                return true;
-        }else{
-            return false;
-        }  
+
+    public double getSaldo(){
+        return Saldo;
     }
-    Scanner s = new Scanner(System.in);
-    
-    
-    boolean transferencia(int x){
-        
-        boolean peticion = true;
-        
-        if(peticion == true){
-                
-            System.out.println("Ingresar el ID de la cuenta a la que se quiere transferir");
-            double IDtransferencia = s.nextDouble(); 
-            Saldo = Saldo - x;
-            double cuatropormil = x*4/1000;
-            Saldo = Saldo - cuatropormil;
-            return true;
-        }else{
-            return false;
-        }  
+    public void setSaldo(double saldo){
+        this.Saldo = saldo;
     }
-    
-    boolean retiro(int x){
-        
-        boolean peticion = true;
-        
-        if(peticion == true){    
-            double cuatropormil = x*4/1000;
-            double totalretiro = x + cuatropormil;
-            if (Saldo< totalretiro){
-                System.out.printl("Su saldo no es suficiente")
-            }else{
-                Saldo = Saldo - totalretiro;
-                System.out.printl("Se realizo exitosamente el retiro");
-                return true;
-        }else{
-            return false;
-        }  
-    }
-    
-    boolean interes(int x){
-        
-        boolean peticion = true;
-        
-        if(peticion == true){
-            Saldo = Saldo * 1.1;
-            System.out.printl("Se genero interes");
-            return true;
-        }else{
-            return false;
-        }  
-    }
-    int getSaldo() {
-        
-        return this.Saldo;
-    }
-    
 }
